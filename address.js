edafbeef081d1cc8799bcfad8f7abc26519ef496
r3alz <chrisjortiz1990@gmail.com>
2021-07-22 13:12:16 -0500

    changed functions to be less lines of code and easier to read in address.js; Also added fallback for undefined components

--- address.js
+++ address.js
@@ -15,10 +15,6 @@
 // CN (China), EG (Egypt), NZ (New Zealand)
 
 // Format 5
-// zip/postal code + cit + sta/province code
-// AM (Armenia), SV (El Salvador)
-
-// Format 6
 // cit
 // BO (Bolivia), 
 
@@ -35,87 +31,84 @@ SV (El Salvador) is formatted: zip-cit then sta is on a different line.  Format 
 
 let addressFormats = {
     format1: ['AL', 'DE', 'DZ', 'AR', 'AT', 'BE', 'BG', 'HR', 'IS', 'DK', 'FR'],
-    format2: ['AU', 'US', 'CA', 'BA', 'DO'],
+    format2: ['AU', 'US', 'CA'],
     format3: ['UK'],
-    format4: ['CN', 'EG', 'NZ'],
-    format5: ['AM', 'SV'],
-    format6: ['BO']
+    format4: ['CN', 'NZ'],
+    format5: ['BO']
 };
 
-// let address1 = "2320 free st."
-// let address2 = "Mesquite, TX, 75150";
-// let country = "Belgium";
-// address2 = address2.split(',').map(item=>item.trim());
-// console.log(address2);
-
-fulladdress = {
-    cit: "Auckland",
-    sta: "",
-    zip: "1024",
-    cot: "",
-    con: "NZ"
+function escapeRegExp(strToEscape) {
+    // Escape special characters for use in a regular expression
+    return strToEscape.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 };
 
-
-function test(address) {
-    addressFormats.format1.forEach(element => {
-        if(element === address.con) {
-            console.log(fulladdress.zip + ' ' + fulladdress.cit);
-            console.log("format 1");
-        }
-     })
-    addressFormats.format2.forEach(element => {
-        if(element === address.con && element === "BA") {
-            console.log(fulladdress.cit + '-' + fulladdress.sta + ', ' + fulladdress.zip);
-            console.log("format 2");
-        }
-        else if(element === address.con && element === "DO") {
-            console.log(fulladdress.cit + ', ' + fulladdress.sta + ', ' + fulladdress.zip);
-            console.log("format 2");
-        }
-        else if(element === address.con) {
-            console.log(fulladdress.cit + ' ' + fulladdress.sta + ' ' + fulladdress.zip);
-            console.log("format 2");
-        }
-    })  
-    addressFormats.format3.forEach(element => {
-        if(element === address.con) {
-            console.log(fulladdress.cit + ' ' + fulladdress.cot + ' ' + fulladdress.zip);
-            console.log("format 3");
-        }
-    })  
-    addressFormats.format4.forEach(element => {
-        if(element === address.con && element === "EG") {
-            console.log(fulladdress.cit + ', ' + fulladdress.zip);
-            console.log("format 4");
-        }
-        else if(element === address.con) {
-            console.log(fulladdress.cit + ' ' + fulladdress.zip);
-            console.log("format 4");
-        }
-    })  
-
-    addressFormats.format5.forEach(element => {
-        if(element === address.con && element === "AM") {
-            console.log(fulladdress.zip + ', ' + fulladdress.cit + ', ' + fulladdress.sta);
-            console.log("format5");
-        }
-        else if(element === address.con && element === "SV") {
-            console.log(fulladdress.zip + '-', fulladdress.cit + ', ' + fulladdress.sta);
-            console.log("format5");
-        }
-        else if(element === address.con) {
-            console.log(fulladdress.zip + ' ' + fulladdress.cit + ' ' + fulladdress.sta);
-            console.log("format 5");
-        }
-    })   
-
-    addressFormats.format6.forEach(element => {
-        if(element === address.con) {
-            console.log(fulladdress.cit);
-            console.log("format 6");
-        }
-    })        
+function trimChar(origString, charToTrim) {
+    charToTrim = escapeRegExp(charToTrim);
+    var regEx = new RegExp("^[" + charToTrim + "]+|[" + charToTrim + "]+$", "g");
+    return origString.replace(regEx, "");
 }
 
-test(fulladdress);
+function test(address) {
+    for(const component in address) {
+        if (typeof(address[component]) === "undefined") {
+            address[component] = "";
+        }
+    }
+
+    if(address.con === "AM") {
+        let format = address.zip + ', ' + address.cit + ', ' + address.sta;
+        format = trimChar(format, ", ");
+        return(format);
+    }
+
+    if(address.con === "BA") {
+        let format = address.cit + '-' + address.sta + ', ' + address.zip;
+        format = trimChar(format, ", ");
+        format = trimChar(format, "-");
+        format = format.replace("-,", ",");
+        return(format);
+    }
+
+    if(address.con === "DO") {
+        let format = address.cit + ', ' + address.sta + ', ' + address.zip;
+        format = trimChar(format, ", ");
+        return(format);
+    }
+
+    if(address.con === "EG") {
+        let format = address.cit + ', ' + address.zip;
+        format = trimChar(format, ", ");
+        return(format);
+    }
+
+    if(address.con === "SV") {
+        let format = address.zip + '-' + address.cit + ', ' + address.sta;
+        format = trimChar(format, ", ");
+        format = trimChar(format, "-");
+        format = format.replace("-,", ",");
+        return(format);
+    }
+
+    if(addressFormats.format1.indexOf(address.con)>=0) {
+        let format = address.zip + ' ' + address.cit;
+        return(format.trim());
+    }
+    if(addressFormats.format2.indexOf(address.con)>=0) {
+        let format = address.cit + ' ' + address.sta + ' ' + address.zip;
+        return(format.trim());
+    }
+    if(addressFormats.format3.indexOf(address.con)>=0) {
+        let format = address.cit + ' ' + address.cot + ' ' + address.zip;
+        return(format.trim());
+    }
+    if(addressFormats.format4.indexOf(address.con)>=0) {
+        let format = address.cit + ' ' + address.zip;
+        return(format.trim());
+    }
+    if(addressFormats.format5.indexOf(address.con)>=0) {
+        let format = address.cit;
+        return(format.trim());
+    }    
+}
+
+
